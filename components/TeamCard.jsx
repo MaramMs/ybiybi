f676fdcce80435265879a4816d3eeaaacfe93efd
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-08-19 13:52:41 +0300

    about us page

---
+++ components/TeamCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TeamCard = () => {
+  return (
+    <div className='bg-[#F5F5F5] h-[492px]  '>
+<img src="/images/team1.png" alt="" />
+<div className='flex justify-center items-center flex-col py-[20px]'>
+    <h3 className='text-[#000] font-[700] text-[18px]'>Joelle Steins</h3>
+    <span className='text-[#aaa] font-[500] text-[13px]'>Senior Developer</span>
+</div>
+    </div>
+  )
+}
+
+export default TeamCard
